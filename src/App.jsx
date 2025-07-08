daa3b230f4192379715d3289845145d9ed3e10ca
JM Carillo <jazellemaira@pm.me>
2025-07-08 20:16:33 +0200

    Refactor App component to use Navbar and remove placeholder content

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 import { gsap } from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from "./components/Navbar"
 
 // Registering GSAP plugins for animations, split-text, and scroll-triggered effects for them to be used globally in the app
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
