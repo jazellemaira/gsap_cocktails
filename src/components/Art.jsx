6a685bfbddc2ec123cc0ea376e20b2a07e905bde
JM Carillo <jazellemaira@pm.me>
2025-07-11 14:42:02 +0200

    fix(image-path): Make image paths relative (/images -> images)

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -47,14 +47,14 @@ const Art = () => {
           <ul className="space-y-4 will-fade">
             {goodLists.map((feature, index) => (
               <li key={index} className="flex items-center gap-2">
-                <img src="/images/check.png" alt="check" />
+                <img src="images/check.png" alt="check" />
                 <p>{feature}</p>
               </li>
             ))}
           </ul>
           <div className="cocktail-img">
             <img
-              src="/images/under-img.jpg"
+              src="images/under-img.jpg"
               alt="cocktail"
               className="abs-center masked-img size-full object-contain"
             />
@@ -62,7 +62,7 @@ const Art = () => {
           <ul className="space-y-4 will-fade">
             {featureLists.map((feature, index) => (
               <li key={index} className="flex items-center justify-start gap-2">
-                <img src="/images/check.png" alt="check" />
+                <img src="images/check.png" alt="check" />
                 <p className="md:w-fit w-60">{feature}</p>
               </li>
             ))}
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -46,15 +46,11 @@ const Contact = () => {
   return (
     <footer id="contact">
       <img
-        src="/images/footer-right-leaf.png"
+        src="images/footer-right-leaf.png"
         alt="right-leaf"
         id="f-right-leaf"
       />
-      <img
-        src="/images/footer-left-leaf.png"
-        alt="left-leaf"
-        id="f-left-leaf"
-      />
+      <img src="images/footer-left-leaf.png" alt="left-leaf" id="f-left-leaf" />
       <div className="content">
         <h2>Where to Find Us</h2>
         <div>
